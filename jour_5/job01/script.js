48c5675a3f01c01e47c25cc9d7f372efe6494969
Tommy <tommy.karotsch@laplateforme.io>
2026-04-02 11:39:41 +0200

    Jour 5 - Job 01

---
+++ jour_5/job01/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const inputPrenom = document.getElementById('prenom');
+    const inputNom = document.getElementById('nom');
+    const inputEmail = document.getElementById('email');
+    const inputAdresse = document.getElementById('adresse');
+    const inputCodePostal = document.getElementById('codepostal');
+    const inputPassword = document.getElementById('password');
+    const inputConfirmPassword = document.getElementById('confirmpassword');
+
+    const msgErreurPrenom = document.getElementById('erreur_prenom');
+    const msgErreurNom = document.getElementById('erreur_nom');
+    const msgErreurEmail = document.getElementById('erreur_email');
+    const msgErreurAdresse = document.getElementById('erreur_adresse');
+    const msgErreurCodePostal = document.getElementById('erreur_codepostal');
+    const msgErreurPassword = document.getElementById('erreur_password');
+    const msgErreurConfirmPassword = document.getElementById('erreur_confirmpassword');
+
+    
+    
+
+});
